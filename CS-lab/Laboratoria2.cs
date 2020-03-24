d7b644216c9b8c8aac447de0de087bec5eb28e3e
Kris Krej <kris@krej.net>
2020-03-24 23:40:03 +0100

    a few Unit Tests

--- CS-lab/Laboratoria2.cs
+++ CS-lab/Laboratoria2.cs
@@ -10,7 +10,7 @@ public class Laboratoria2 {
         return 0;
     }
 
-    public bool IsGreaterThanZero() {
+    public bool IsGreaterThanZero(int value) {
         return false;
     }
 
--- Tests/Lab2Tests.cs
+++ Tests/Lab2Tests.cs
@@ -5,10 +5,33 @@ namespace Tests {
         Laboratoria2 lab2 = new Laboratoria2();
 
         [Test]
-        public void AddTests() {
+        public void Zad1_AddTests() {
             Assert.AreEqual(0, lab2.Add(0, 0), "0+0=0");
             Assert.AreEqual(10, lab2.Add(5, 5), "5+5=10");
             Assert.AreEqual(-3, lab2.Add(-10, 7), "-10+7=-3");
+        }
+
+        [Test]
+        public void Zad2_IsGreaterThanZeroTests() {
+            Assert.AreEqual(false, lab2.IsGreaterThanZero(-4), "-4<0");
+            Assert.AreEqual(false, lab2.IsGreaterThanZero(0), "0==0");
+            Assert.AreEqual(true, lab2.IsGreaterThanZero(3), "3>0");
+            Assert.AreEqual(true, lab2.IsGreaterThanZero(6), "6>0");
+        }
+
+        [Test]
+        public void Zad3_GetOfficialGreetingTests() {
+            Assert.AreEqual("Towarzyszy nam dziœ pan Adam", lab2.GetOfficialGreeting("Adam"));
+            Assert.AreEqual("Towarzyszy nam dziœ pan Aleks", lab2.GetOfficialGreeting("Aleks"));
+            Assert.AreEqual("Towarzyszy nam dziœ pani Zosia", lab2.GetOfficialGreeting("Zosia"));
+            Assert.AreEqual("Towarzyszy nam dziœ pani Renata", lab2.GetOfficialGreeting("Renata"));
+        }
+
+        [Test]
+        public void Zad4_AreEqual() {
+            Assert.AreEqual(true, lab2.AreEqual(0, 0), "0==0");
+            Assert.AreEqual(true, lab2.AreEqual(5, 5), "5==5");
+            Assert.AreEqual(false, lab2.AreEqual(10, 7), "10!=7");
         }
     }
 }
