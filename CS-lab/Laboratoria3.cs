24ec4e25a1fbd3cb3576b6b76ac515b827d26077
Kris Krej <kris@krej.net>
2020-03-26 21:26:36 +0100

    Dictionaries + lab3

---
+++ CS-lab/Laboratoria3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Laboratoria3 {
+    /*
+     * Todo:
+     *   * Wypisz wszystkie słowa ze słownika
+     *   * To samo, ale pętlą foreach
+     *   * Wypisz wszystkie zaczynające się na "ó"
+     *   * Wypisz wszystkie rymy do danego słowa
+     *   * Wypisz wszystkie rymy do podanego przez użytkownika słowa
+     *   * Wypisz najdłuższe słowo w słowniku.
+     *   * Sprawdź, czy użytkownik nie zaklął
+     *   * Znajdź słowa najtrudniejsze dla dyslektyków
+     */
+
+}
--- CS-lab/Program.cs
+++ CS-lab/Program.cs
@@ -2,7 +2,7 @@
 
 class Program {
     static void Main() {
-        var lab2 = new Laboratoria2();
-        lab2.ShowIntroduction();
+        var lab3 = new Laboratoria3();
+
     }
 }
